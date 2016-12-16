ee3debf0a2788d86f76f4feab2a6bfc57c28ad07
loolys <pierre.liljekvist@outlook.com>
2016-12-16 16:36:27 +0100

    Set up a simple express server that returns the users ip, os, and language in json format

---
+++ README.md
@@ -0,0 +1,5 @@
+# Request-Headers-Microservice
+
+This service uses node.js and express to get your ip-address, operating System
+
+and browser language.
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require ('express');
+const os = require('os');
+
+var app = express();
+function data(ip, oper, lang) {
+  return {
+    "ip": ip,
+    "Operating System": oper,
+    "Language": lang,
+  };
+}
+app.get('/', function(req, res) {
+  let jason = data(req.ip, os.platform(), req.acceptsLanguages()[0]);
+  let obj = JSON.stringify(jason, null, 2);
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(obj);
+});
+let port = process.env.PORT || 5000;
+app.listen(port);
